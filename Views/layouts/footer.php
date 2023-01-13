a6525c4fe27dcfc72aff05db2f7a393329bdf32b
m.sylwester <m.sylwester@outlook.com>
2023-01-13 21:14:36 +0100

    Better template handling

--- Views/layouts/footer.php
+++ Views/layouts/footer.php
@@ -1,3 +1,7 @@
-<script src="<?= asset('js/bootstrap.js') ?>"></script>
-</body>
+            <footer class="mt-auto text-white-50">
+                <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
+            </footer>
+        </div>
+        <script src="<?= asset('js/bootstrap.js') ?>"></script>
+    </body>
 </html>
